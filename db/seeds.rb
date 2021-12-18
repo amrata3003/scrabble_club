e1fdd5b44807ac63e24c7c0274762c4e9ca59b8a
Amrata Pathak <amrata3003@gmail.com>
2021-12-18 09:33:38 +0530

    Added fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+date = DateTime.current.at_beginning_of_year + 8.hours
+5.times do 
+  Player.create(
+  	name: Faker::Name.unique.name,
+  	mobile_number: Faker::PhoneNumber.cell_phone,
+  	email: Faker::Internet.email,
+  	joining_date: date + Faker::Number.between(from: 8, to: 10).hours
+  )
+end
+
+Game.create(name: 'Football')
+Game.create(name: 'Cricket')
+Game.create(name: 'Ludo')
+
+25.times do 
+  Participation.create(
+    game: Game.all.sample,
+    player: Player.all.sample,
+    opponent: Player.all.sample,
+    score: Faker::Number.between(from: 30, to: 100)
+  )
+end
